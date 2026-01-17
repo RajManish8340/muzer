5e95a0d16dc133ec54285ca725a58ad224bce2cc
Raj pop <rajmanishsahu1121@gmail.com>
2026-01-17 22:14:05 +0530

    init in pop

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 
 export default function Home() {
-   return (
+  return (
+    <div>
       <div className="flex justify-between p-5 bg-blue-400 ">
-         <div>Hello world</div>
-         <div>my name is raj maish sahu</div>
+        <div>Hello world</div>
+        <div>my name is raj maish sahu</div>
       </div>
 
-   );
+    </div>
+  );
 }
