3e3452aa4edc27fb96c82f59dc0318e1a91a3914
Raj pop <rajmanishsahu1121@gmail.com>
2026-01-18 16:59:04 +0530

    some schema

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
         <div>Hello world</div>
         <div>my name is raj maish sahu</div>
       </div>
-
     </div>
   );
 }
--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,23 @@
-import { varchar, pgTable, integer } from "drizzle-orm/pg-core";
+import { varchar, pgTable, integer, pgEnum } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+
+const roleEnum = pgEnum("role", ["normal", "admin"])
 
 export const userTable = pgTable("users", {
-   id: integer("id").primaryKey(),
-   name: varchar('name').notNull(),
-   email: varchar("email").notNull()
+  id: integer("id").primaryKey(),
+  name: varchar('name').notNull(),
+  email: varchar("email").notNull(),
+  role: roleEnum()
 })
+
+export const adminTable = pgTable("admins", {
+  id: integer("id").primaryKey(),
+  role: roleEnum().$default(() => "admin")
+})
+
+export const AdminsRelations = relations(adminTable, ({ one }) => ({
+  admin: one(userTable, {
+    fields: [adminTable.id],
+    references: [userTable.id],
+  })
+}))
